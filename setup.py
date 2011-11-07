d6d44644073bb4acb4f8a68bb92e82f07431fc28
Micha Gorelick <mynameisfiber@gmail.com>
2011-11-06 19:44:29 -0500

    Added more keyboards to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ import shutil
 
 setup(
   name = "bitcpy",
+  description="Utility to shorten links in your clipboard",
+  long_description=open("./README.md", "r").readlines(),
   version = "0.2",
+  author="Micha Gorelick",
+  author_email="mynameisfiber@gmail.com",
+  url="https://github.com/mynameisfiber/bitcpy",
   packages=['bitcpy'],
   install_requires = ['xerox>=0.2.1'],
   dependency_links = ['git://github.com/bitly/bitly-api-python.git#egg=bitly_api'],
